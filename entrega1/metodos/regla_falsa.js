8056fd8ab03a6bf2d0b52a6a7826f939e0bcaafb
dsolanov <73181151+dsolanov@users.noreply.github.com>
2020-10-25 22:34:27 -0500

    Add files via upload

---
+++ entrega1/metodos/regla_falsa.js
@@ -0,0 +1,61 @@
+import { Util } from './util.js'
+import * as math from 'mathjs'
+
+export const regla_falsa = {
+
+    evaluate: (f, xi, xs, iter, tol, error) => {
+        const parser = math.parser();
+        parser.evaluate("f(x)=" + f);
+        var table = [], msg,
+            fxi = parser.evaluate("f(" + xi + ")"),
+            fxs = parser.evaluate("f(" + xs + ")");
+        if (0 === fxi)
+            msg = "Xi = " + xi + " es una raíz";
+        else if (0 === fxs)
+            msg = "Xu = " + xs + " es una raíz";
+        else if (fxi * fxs < 0) {
+            var xa, xm = xi - fxi * (xs - xi) / (fxs - fxi),
+                fxm = parser.evaluate("f(" + xm + ")"),
+                n = 0, e = tol + 1;
+            table.push([
+                n,
+                Util.fixed(xi),
+                Util.fixed(xs),
+                Util.fixed(xm),
+                Util.fixedExp(fxm), ""
+            ])
+            while (e > tol && 0 !== fxm && n < iter - 1) {
+                if (fxi * fxm < 0) {
+                    xs = xm;
+                    fxs = fxm;
+                } else {
+                    xi = xm;
+                    fxi = fxm;
+                }
+                xa = xm;
+                xm = xi - fxi * (xs - xi) / (fxs - fxi);
+                fxm = parser.evaluate("f(" + xm + ")");
+                e = Math.abs(xm - xa);
+                if (error === "1") e /= xm;
+                n += 1;
+                table.push([
+                    n,
+                    Util.fixed(xi),
+                    Util.fixed(xs),
+                    Util.fixed(xm),
+                    Util.fixedExp(fxm),
+                    Util.fixedExp(e)
+                ]);
+            }
+            0 === fxm
+                ? msg = "Aproximación a la raíz $x_m=" + xm + "$ donde $f(Xm)=0$"
+                : e < tol
+                    ? msg = "Aproximación a la raíz $x_m=" + xm + "$ con $Error=" + e + "$"
+                    : msg = "Fracaso en " + iter + " iteraciones, hasta el momento $x_m=" + xm + "$"
+        } else {
+            msg = "El intervalo es inadecuado, encuentre uno con cambio de signo en $f(x)$";
+        }
+        return { table, msg }
+    }
+
+}

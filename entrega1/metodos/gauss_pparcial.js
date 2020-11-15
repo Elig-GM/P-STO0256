207cb04090de30f6ed4cadbc0f9c5ffdd5e46b8d
Elig-GM <emosque1@eafit.edu.co>
2020-11-15 01:22:00 -0500

    Mejoras en los métodos

--- entrega1/metodos/gauss_pparcial.js
+++ entrega1/metodos/gauss_pparcial.js
@@ -6,50 +6,49 @@ const gauss_pparcial = {
         let Matrix = math.matrix(array);
         let matrix = Matrix._data,
             n = Matrix._size[0],
-            mults,
-            max = {
+            mults;
+
+        console.log("Etapa 0");
+        console.table(matrix);
+        for (let i = 0; i < n - 1; i++) {
+            let max = {
                 pos: 0,
                 val: 0,
             };
-        
-        for (let i = 0; i < n; i++) {
-            if ( matrix[i][0] > max.val ) {
-                max.val = matrix[i][0];
-                max.pos = i;
+            for (let j = i; j < n; j++) {
+                if (Math.abs(matrix[j][i]) > Math.abs(max.val)) {
+                    max.val = matrix[j][i];
+                    max.pos = j;
+                }
             }
-        }
+            if (max.pos > 0)
+                [matrix[max.pos], matrix[i]] = [matrix[i], matrix[max.pos]];
 
-        if (max.pos > 0) {
-            [matrix[max.pos], matrix[0]] = [matrix[0], matrix[max.pos]];
-        }
-
-        for (let i = 0; i < n-1; i++) {
-            console.log("Etapa " + i);
-            console.log(matrix);
             mults = [];
             for (let j = i + 1; j < n; j++) {
-                mults.push(matrix[j][i] / matrix[i][i]);
+                mults.push(math.divide(matrix[j][i], matrix[i][i]));
             }
-            Matrix.subset(math.index(math.range(i, n), math.range(0, n+1)))
-                // eslint-disable-next-line no-loop-func
+            Matrix.subset(math.index(math.range(i, n), math.range(0, n + 1)))
                 .forEach(function (value, xy) {
                     if (xy[0] >= 1) {
-                        matrix[ xy[0] + i ][ xy[1] ] = value - mults[ xy[0]-1 ] * matrix[ xy[0] + ( i - xy[0] ) ][ xy[1] ];
+                        matrix[xy[0] + i][xy[1]] = value - mults[xy[0] - 1] * matrix[xy[0] + (i - xy[0])][xy[1]];
                     }
                 });
+            console.log("Etapa " + i + 1);
+            console.table(matrix);
         }
-        
+
         let x = [];
         let temp = 0;
-        x.push(matrix[n-1][n]/matrix[n-1][n-1]);
+        x.push(matrix[n - 1][n] / matrix[n - 1][n - 1]);
         for (let i = n - 2; i > -1; i--) {
             temp = matrix[i][n];
-            for (let j = i+1, k = x.length-1; j < n; j++, k --) {
-                temp -=  matrix[i][j] * x[k];
+            for (let j = i + 1, k = x.length - 1; j < n; j++, k--) {
+                temp -= matrix[i][j] * x[k];
             }
-            x.push( temp / matrix[i][i] );
+            x.push(temp / matrix[i][i]);
         }
-        x.reverse().map((v, i) => console.log("x"+i+": "+v));
+        x.reverse().map((v, i) => console.log("x" + i + ": " + v));
         return { matrix, x }
     }
 };
--- entrega1/metodos/gauss_ptotal.js
+++ entrega1/metodos/gauss_ptotal.js
@@ -1,74 +1,71 @@
-// // import * as math from "mathjs";
 var math = require("mathjs");
 
-let numMaxRow = [];
-const gaus_ptotal = {
-    
+let posVars = [];
+const gauss_ptotal = {
+
     evaluate: (array) => {
         let Matrix = math.matrix(array);
-        let matrix = Matrix._data,
-            n = Matrix._size[0],
-            mults;
-            
-        gaus.pivoteo(Matrix, n, 0);
-        for (let i = 0; i < n-1; i++) {
-            console.log("Etapa " + i);
-            console.log(matrix);
+        let matrix = Matrix._data, n = Matrix._size[0], mults;
+
+        posVars = [...Array(n).keys()];
+
+        console.log("Etapa 0");
+        console.table(matrix);
+
+        for (let i = 0; i < n - 1; i++) {
+            gauss_ptotal.pivoteo(Matrix, n, i);
             mults = [];
             for (let j = i + 1; j < n; j++) {
-                mults.push(matrix[j][i] / matrix[i][i]);
+                mults.push(math.divide(matrix[j][i], matrix[i][i]));
             }
-            Matrix.subset(math.index(math.range(i, n), math.range(0, n+1)))
-                // eslint-disable-next-line no-loop-func
-                .forEach(function (value, xy) {
+            Matrix.subset(math.index(math.range(i, n), math.range(0, n + 1)))
+                .map((value, xy) => {
                     if (xy[0] >= 1) {
-                        matrix[ xy[0] + i ][ xy[1] ] = value - mults[ xy[0]-1 ] * matrix[ xy[0] + ( i - xy[0] ) ][ xy[1] ];
+                        matrix[xy[0] + i][xy[1]] = math.add(value, -math.multiply(mults[xy[0] - 1], matrix[xy[0] + (i - xy[0])][xy[1]]));
                     }
                 });
-            gaus.pivoteo(Matrix, n, i+1);
-            console.log(matrix);
+            console.log("Etapa " + i + 1);
+            console.table(matrix);
         }
-        
-        let x = new Array(4).fill(0);
+
+        let x = [];
         let temp = 0; 
-        let firtsRow = math.row(matrix, 0)[0];
-        x[firtsRow.findIndex(e => (e === numMaxRow[0]))] = matrix[n-1][n]/matrix[n-1][n-1];
+        posVars.reverse();
+        x[posVars[0]] = matrix[n-1][n]/matrix[n-1][n-1];
         for (let i = n - 2, f = 1; i > -1; i--, f++) {
             temp = matrix[i][n];
             for (let j = i,  r = f-1 ; j < i + f; j++, r--) {
-                temp -=  matrix[i][n-1-r] * x[firtsRow.findIndex(e => (e === numMaxRow[r]))];
+                temp -=  matrix[i][n-1-r] * x[posVars[r]];
             }
-            x[firtsRow.findIndex(e => (e === numMaxRow[f]))] = temp / matrix[i][i];
+            x[posVars[f]] = temp / matrix[i][i];
         }
 
-        x.reverse().map((v, i) => console.log("x"+i+": "+v));
+        x.map((v, i) => console.log("x"+i+": "+v));
         return { matrix, x }
     },
 
-    pivoteo: (Matrix, x, j) => {
+    pivoteo: (Matrix, n, j) => {
+        let matrix = Matrix._data;
         let max = {
             pos: [],
             val: 0,
         };
-        let matrix = Matrix._data;
 
-        Matrix.subset(math.index(math.range(0, x), math.range(j, x))).forEach(function (value, xy) {
-            if ( value > max.val ) {
+        Matrix.subset(math.index(math.range(j, n), math.range(j, n))).forEach(function (value, xy) {
+            if (Math.abs(value) > Math.abs(max.val)) {
                 max.pos = xy;
                 max.val = value;
             }
         });
-        
-        if (max.pos[0] > 0) {
-            [matrix[max.pos[0]], matrix[j]] = [matrix[j], matrix[max.pos[0]]];
-            if (j === 0)
-                numMaxRow = [...math.row(matrix, 0)[0]];
-            max.col = math.column(matrix, max.pos[1]+j);
-            if (max.pos[1] > 0) {
-                for (let i = 0; i < x; i++) {
-                    [matrix[i][max.pos[1] + j], matrix[i][j]] = [matrix[i][j], matrix[i][max.pos[1] + j]];
-                }
+
+        if (max.pos[0] + j !== j)
+            [matrix[max.pos[0] + j], matrix[j]] = [matrix[j], matrix[max.pos[0] + j]];
+
+        if (max.pos[1] + j !== j) {
+            for (let i = 0; i < n; i++) {
+                [matrix[i][max.pos[1] + j], matrix[i][j]] = [matrix[i][j], matrix[i][max.pos[1] + j]];
             }
+            [posVars[max.pos[1] + j], posVars[j]] = [posVars[j], posVars[max.pos[1] + j]]
         }
     }
 
